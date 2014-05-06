d7efb339e82bb9bb22882e639a9c6abd1946fb36
Raghu Betina <git@rugb.com>
2014-05-05 20:11:09 -0500

    Started many-to-many between actors and movies

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,5 @@ class Actor < ActiveRecord::Base
   validates :name, :uniqueness => true
 
   has_many :roles
+  has_many :movies, :through => :roles
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,19 @@ class Movie < ActiveRecord::Base
 
   belongs_to :director
   has_many :roles
+  has_many :actors, :through => :roles
 
   # def director
   #   return Director.find(director_id)
+  # end
+
+  # def actors
+  #   results = []
+
+  #   roles.each do |role|
+  #     results.push(role.actor)
+  #   end
+
+  #   return results
   # end
 end
