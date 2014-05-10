43c3b7199c5abdecbc698216969274e182d3f0b0
Todd S <starco58@gmail.com>
2014-05-10 13:07:38 -0500

    set-up complete
    bundle install
    rake db:migrate
    rake db:seed
    dbs set up and data populated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506141549) do
+ActiveRecord::Schema.define(version: 20140510130456) do
 
   create_table "actors", force: true do |t|
     t.string "dob"
