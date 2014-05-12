300c3301cbbb518f38d9d8db69db201e7b4039ba
Todd S <starco58@gmail.com>
2014-05-12 08:05:10 -0500

    attempting commit of q3 again
    using the suggestion made by SS

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,31 +33,23 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    top_director = Director.new
 
-      Director.all.each do |the_director|
-        if the_director.movies.count > top_director.movies.count
 
-        top_director = the_director
+      movie_counts = []
+
+        Director.all.each do |the_director|
+
+        movies_directed_by_the_director = the_director.movies.count
+
+        movie_counts.push(movies_directed_by_the_director)
+
       end
-
-    end
-
-      # movie_counts = []
-
-      #   Director.all.each do |the_director|
-
-      #   movies_directed_by_the_director = the_director.movies.count
-
-      #   movie_counts.push(movies_directed_by_the_director)
-
-      # end
 
     #   @most_number_of_movies = movie_counts.sort.last
 
     # @director_with_the_most_movies = Director.find(@most_number_of_movies).name
 
-    @director_with_the_most_movies = top_director.name
+    @director_with_the_most_movies = the_director.movies.count
   end
 
   def question_4
